56c2f2589b32a24d24494bc9f8af6278ad051315
GAYDAMAKHA MIKHAIL <mikhail.gaydamakha@etu.unistra.fr>
2018-12-13 21:51:53 +0100

    updating count

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -10,7 +10,7 @@ class CartController extends Controller
 {
     public static function count(Request $request){
         if($request->session()->has('cart_count'))
-            return response()->json($request->session()->get('cart_count'));
+            return response()->json(['count'=>$request->session()->get('cart_count')]);
         else
             return response()->json(['count' => 0]);
     }
@@ -18,8 +18,11 @@ class CartController extends Controller
         $article = Article::where('id',$article_id)
                             ->get();
         if(!empty($article)){
-            $request->session()->put('cart_count',1);
-            
+            $count=0;
+            if($request->session()->has('cart_count'))
+                $count=$request->session()->get('cart_count');
+            $count=$count+1;
+            $request->session()->put('cart_count',$count);     
         }
     }
 }
