202e68347500eee88ef79aacadf54a8a3d9d8617
GAYDAMAKHA MIKHAIL <mikhail.gaydamakha@etu.unistra.fr>
2018-12-13 09:58:08 +0100

    getArticles

--- app/Http/Controllers/ArticleComtroller.php
+++ app/Http/Controllers/ArticleComtroller.php
@@ -8,7 +8,10 @@ use PDO;
 class ArticleController extends Controller
 {
     public static function getArticlesByCategory($category){
-        
+        $articles = Article::select('name','category','price','avalaible')
+								->where('category','like',$category)
+                                ->get();
+        return response()->json($articles);
     }
 }
 ?>
