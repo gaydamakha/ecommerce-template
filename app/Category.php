ceb7443c4663c2a5e60641142e0e1a49aaefba78
GAYDAMAKHA MIKHAIL <mikhail.gaydamakha@etu.unistra.fr>
2018-12-13 10:44:51 +0100

    s

--- app/Category.php
+++ app/Category.php
@@ -17,12 +17,13 @@ class Category extends Model {
     public static function getAllCategoriesByParent($category){
         $categories=self::select('name')
                         ->where('name','like',$category);
+        echo implode('|',$categories);
         if(!empty($categories)){
             foreach($categories as $cat){
                 $categories[] = self::getAllCategoriesByParent($cat['name']);
             }
         }
-        echo implode('|',$categories);
+        //echo implode('|',$categories);
         return $categories;
     }
     // Relationships
