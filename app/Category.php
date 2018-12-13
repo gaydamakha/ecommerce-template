20f4b36481f0a109d7980fbd8a0b7cc84fc308a8
GAYDAMAKHA MIKHAIL <mikhail.gaydamakha@etu.unistra.fr>
2018-12-13 11:12:12 +0100

    get added

--- app/Category.php
+++ app/Category.php
@@ -16,14 +16,15 @@ class Category extends Model {
     ];
     public static function getAllCategoriesByParent($category){
         $categories=self::select('name')
-                        ->where('name','like',$category);
+                        ->where('name','like',$category)
+                        ->get();
         echo implode("|",$categories);
         if(!empty($categories)){
             foreach($categories as $cat){
                 $categories[] = self::getAllCategoriesByParent($cat['name']);
             }
         }
-        //echo implode('|',$categories);
+        echo implode('|',$categories);
         return $categories;
     }
     // Relationships
