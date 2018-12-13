4ce2f23d416ca8ddbd4b8f977e25bae7296b0052
GAYDAMAKHA MIKHAIL <mikhail.gaydamakha@etu.unistra.fr>
2018-12-13 10:31:15 +0100

    ss

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -9,7 +9,7 @@ class ArticleController extends Controller
 {
     public static function getArticlesByCategory($category){
         $categories = Category::getAllCategoriesByParent($category);
-        echo $categories;
+        echo implode('|',$categories);
         $articles = Article::select('name','category','price','avalaible')
 								->where('category','in',$categories)
                                 ->get();
