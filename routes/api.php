504adf488533ace61db5fe7f4e74c18cc608a6e1
GAYDAMAKHA MIKHAIL <mikhail.gaydamakha@etu.unistra.fr>
2018-12-13 09:51:02 +0100

    routes for products

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,22 @@
 <?php
 
-use App\Http\Controllers\CategoryController as cc;
+use App\Http\Controllers\CategoryController as Cc;
+use App\Http\Controllers\ArticleController as Ac;
 
 $router->get('/',  function () {
     return ['pop','pip'];
 });
+$router->group(['prefix' => 'products'], function () use ($router) {
+	$router->get('/',  function () {
+    	return;
+	});
+	$router->get('/{category}',  function ($category) {
+    	return Ac::getArticlesByCategory($category);
+	});
+});
 $router->group(['prefix' => 'categories'], function () use ($router) {
 	$router->get('/',  function () {
-    	return cc::getList();
+    	return Cc::getList();
 	});
 });
 ?>
