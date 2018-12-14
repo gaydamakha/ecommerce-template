a7a550a4217a732e869f69a83e03edc0afa08168
GAYDAMAKHA MIKHAIL <mikhail.gaydamakha@etu.unistra.fr>
2018-12-14 17:11:01 +0100

    web for all

--- routes/api.php
+++ routes/api.php
@@ -16,37 +16,32 @@ use App\Http\Controllers\CartController as CartC;
 |
 */
 
-// Route::middleware('auth:api')->get('/user', function (Request $request) {
-//     return $request->user();
-// });
-
-Route::get('/',  function () {
-    return ['pop','pip'];
-});
-Route::group(['prefix' => 'products'], function () use ($router) {
-	Route::get('/',  function () {
-    	return Ac::getAll();
+Route::middleware(['web'])->group(function() use($router){
+	Route::group(['prefix' => 'products'], function () use ($router) {
+		Route::get('/',  function () {
+			return Ac::getAll();
+		});
+		Route::get('/{category_id}',  function ($category_id) {
+			return Ac::getArticlesByCategory($category_id);
+		});
 	});
-	Route::get('/{category_id}',  function ($category_id) {
-    	return Ac::getArticlesByCategory($category_id);
+	Route::group(['prefix' => 'categories'], function () use ($router) {
+		Route::get('/',  function () {
+			return Cc::getList();
+		});
 	});
-});
-Route::group(['prefix' => 'categories'], function () use ($router) {
-	Route::get('/',  function () {
-    	return Cc::getList();
-	});
-});
-Route::group(['prefix' => 'cart','middleware'=>['web']], function () use ($router) {
-	Route::get('/count',  function (Request $request) {
-		return CartC::count($request);
-	});
-	Route::post('/add_article',  function (Request $request) {
-		return CartC::addToCart($request);
-	});
-	Route::post('/remove_article',  function (Request $request) {
-		return CartC::removeFromCart($request);
-	});
-	Route::get('/get_cart',  function (Request $request) {
-		return CartC::getAll($request);
+	Route::group(['prefix' => 'cart',], function () use ($router) {
+		Route::get('/count',  function (Request $request) {
+			return CartC::count($request);
+		});
+		Route::post('/add_article',  function (Request $request) {
+			return CartC::addToCart($request);
+		});
+		Route::post('/remove_article',  function (Request $request) {
+			return CartC::removeFromCart($request);
+		});
+		Route::get('/get_cart',  function (Request $request) {
+			return CartC::getAll($request);
+		});
 	});
 });
